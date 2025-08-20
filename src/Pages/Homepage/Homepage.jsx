143d77ac889ed0fedc5a4462fef766e4d1961b96
Yash Raj <yashprojects123@gmail.com>
2025-08-20 19:20:02 +0530

    Uploaded latest code

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -14,7 +14,7 @@ import { data } from 'react-router-dom';
 const Homepage = () => {
   let [pageData, setPageData] = React.useState({
     loading: true,
-    data: null,
+    data: [],
     error:false
   });
   let [pageError, setPageError] = React.useState("");
@@ -48,6 +48,8 @@ const Homepage = () => {
   }
 fetchHomepageContent()
 },[]);
+const sections = Array.isArray(pageData?.data?.sections) ? pageData.data.sections : [];
+
   let descriptionTexts = [
     `<p>The buzzing capital of Denmark mixes <a className="link-internal" data-entity-type="node" data-entity-uuid="3d82ffe2-afd3-46f9-9abe-ba9aa0159c3a" data-entity-substitution="canonical" title="Experience world-class modern architecture" href="/copenhagen/activities/experience-world-class-architecture-and-design">modern architecture</a> and <a className="link-internal" data-entity-type="node" data-entity-uuid="4afafbe9-735f-476f-bf98-9cdda4dd6740" data-entity-substitution="canonical" title="See and do" href="/copenhagen/activities/see-and-do">culture </a>with <a className="link-internal" data-entity-type="node" data-entity-uuid="8c5c1a23-45b4-496b-b270-b86745baac23" data-entity-substitution="canonical" title="A sustainability guide to Copenhagen" href="/copenhagen/activities/green-sustainability-guide">sustainable living</a>, <a className="link-internal" data-entity-type="node" data-entity-uuid="01642282-dd53-4812-99a4-cdb44c2429f7" data-entity-substitution="canonical" title="The royal and historic Copenhagen" href="/copenhagen/activities/royal-and-historic-copenhagen">royal history</a>, and a mouthwatering <a className="link-internal" data-entity-type="node" data-entity-uuid="88b8db61-c17f-43b4-a519-e9dc5ebe9884" data-entity-substitution="canonical" title="Restaurants in Copenhagen" href="/best-restaurants-copenhagen">restaurant scene</a>. Get to know the city, do your planning, and find all your questions answered right here.</p>`,
     `<p>To get a daily dose of inspiration, please follow <a className="link-external" target="_blank" rel="noopener" href="https://www.instagram.com/visitcopenhagen/">@VisitCopenhagen</a> on Instagram.</p>
@@ -56,32 +58,38 @@ fetchHomepageContent()
 
   return (
     <div className="homepage-content">
-      { pageData.loading && <div className="loading">Loading...</div> }
-      { pageData.error && <div className="error">Error: {pageError}</div> }
-      { pageData.data && 
-      pageData.data.sections.map((section, index) => {
-        (section.data.type ==='banner') ?
-          <h1 key={index} className="homepage-title">
-            {item.title}
-          </h1>
-          : <p>Other section type</p>;
-      })
-      }
-      {/* Banner Section  */}
-
-      <Suspense fallback={<div className="skeleton-banner">
-        <div className="container">
-          <div className='images'>
-            <div className="skeleton-image" />
-            <div className="skeleton-image" />
-          </div>
-        </div>
-      </div>}>
-
-        <Banner className="homepage-banner" mainTitle="Get to know Copenhagen, the capital of Denmark" caption="Mark Tanggaard & Astrid Maria Rasmussen"
-          subTitle="Your guide to the perfect Copenhagen experience" descriptionTexts={descriptionTexts} image1={image1} image2={image2} />
-
-      </Suspense>
+<div className="sections">
+  {sections &&
+    sections.map(({ type, data }, index) =>
+      type === 'banner' ? (
+        <Suspense
+          key={index}
+          fallback={
+            <div className="skeleton-banner">
+              <div className="container">
+                <div className="images">
+                  <div className="skeleton-image" />
+                  <div className="skeleton-image" />
+                </div>
+              </div>
+            </div>
+          }
+        >
+          <Banner
+            className="homepage-banner"
+            mainTitle={data.topic}
+            caption={data.caption}
+            subTitle={data.subTopic}
+            descriptionTexts={data.description}
+            images={data.images.urls}
+          />
+        </Suspense>
+      ):null
+    )
+  }
+</div>
+      
+     
 
       {/* Tour interest Section  */}
       <TourInterestSection />
