33c9808d5ede274f6b4a164622aed00f75db32fe
Yash Raj <yashprojects123@gmail.com>
2025-08-20 19:21:07 +0530

    Uploaded latest codes

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,27 +2,35 @@ import React from 'react'
 import './Banner.css'
 import { Link } from 'react-router-dom';
 import parse from 'html-react-parser';
-const Banner = ({ className, image1, image2, mainTitle, caption, subTitle, descriptionTexts, location }) => {
+const Banner = ({ className, images, mainTitle, caption, subTitle, descriptionTexts, location }) => {
+
+        const safeDescriptions = Array.isArray(descriptionTexts)
+                ? descriptionTexts
+                : descriptionTexts
+                        ? [descriptionTexts] // wrap single string in array
+                        : [];
+        console.log(safeDescriptions)
         return (
                 <div className={`banner-section-wrapper ${className}`}>
                         <div className="container">
                                 <div className='banner-content'>
-                                        {(image1 && image2) ?
-                                                <div className="images">
-                                                        <div>
-                                                                <img src={image1} alt={mainTitle} loading='eager'/>
+                                        {Array.isArray(images) && images.length > 0 ? (
+                                                images.length > 1 ? (
+                                                        <div className="images">
+                                                                {images.map((src, index) => (
+                                                                        <div key={index}>
+                                                                                <img src={src.startsWith("http") ? src : `${import.meta.env.VITE_BACKEND_URL_FOR_IMAGES}${src}`} alt={mainTitle} loading="eager" />
+                                                                        </div>
+                                                                ))}
                                                         </div>
-                                                        <div>
-                                                                <img src={image2} alt={mainTitle} loading='eager' />
+                                                ) : (
+                                                        <div className="banner-image">
+                                                                <div>
+                                                                        <img src={images[0].startsWith("http") ? images[0] : `${import.meta.env.VITE_BACKEND_URL_FOR_IMAGES}${images[0]}`} alt={mainTitle} loading="eager" />
+                                                                </div>
                                                         </div>
-                                                </div>
-                                                :
-                                                <div className="banner-image">
-                                                        <div>
-                                                                <img src={image1} alt={mainTitle} loading='eager' />
-                                                        </div>
-                                                </div>
-                                        }
+                                                )
+                                        ) : null}
 
                                         <div className="banner-title text-center">
                                                 <h1>{mainTitle}</h1>
@@ -34,9 +42,10 @@ const Banner = ({ className, image1, image2, mainTitle, caption, subTitle, descr
                                                 <h2 className='subTitle'>{subTitle}</h2>
                                                 <div className='banner-texts'>
                                                         {
-                                                                descriptionTexts.map((item, index) => {
+                                                                safeDescriptions.map((item, index) => {
                                                                         return <div key={index}>{parse(item)}</div>
                                                                 })
+
                                                         }
 
                                                 </div>
@@ -45,8 +54,8 @@ const Banner = ({ className, image1, image2, mainTitle, caption, subTitle, descr
 
                                 </div>
                         </div>
-                        </div>
-                        )
+                </div>
+        )
 }
 
-                        export default Banner
+export default Banner
